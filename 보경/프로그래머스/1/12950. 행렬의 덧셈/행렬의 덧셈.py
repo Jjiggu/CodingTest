5a6093b5877fee27b1fa01ef2c342552cd07817d
seobokyeong0314 <116994319+seobokyeong0314@users.noreply.github.com>
2024-08-11 22:16:38 +0900

    Fix: [프로그래머스-12950] 행렬의 덧셈

--- 보경/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 보경/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,8 +1,6 @@
+import numpy as np
 def solution(arr1, arr2):
-    answer = []
-    for arr_1, arr_2 in zip(arr1, arr2):
-        temp = []
-        for a1, a2 in zip(arr_1, arr_2):
-            temp.append(a1+a2)
-        answer.append(temp)
-    return answer
+    arr1 = np.array(arr1)
+    arr2 = np.array(arr2)
+    answer = arr1 + arr2
+    return answer.tolist()

81c857d2fabdaa23b618898dc987dfe2525eca98
seobokyeong0314 <116994319+seobokyeong0314@users.noreply.github.com>
2024-08-11 22:12:59 +0900

    Fix: [프로그래머스-86051] 없는 숫자 더하기

--- 보경/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 보경/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -2,6 +2,5 @@ def solution(numbers):
     answer = -1
     num_lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in numbers:
-        if i in num_lst:
-            num_lst.remove(i)
+        num_lst.remove(i)
     return sum(num_lst)

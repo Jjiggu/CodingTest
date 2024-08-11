07f3faa2b84e91bc03d899f3eba1d2bf0ea4812f
seobokyeong0314 <116994319+seobokyeong0314@users.noreply.github.com>
2024-08-11 22:13:28 +0900

    Fix: [프로그래머스-12917] 문자열 내림차순으로 배치하기

--- 보경/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 보경/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -1,6 +1,3 @@
 def solution(s):
-    answer = ''
-    s_new = sorted(s, reverse = True)
-    for word in s_new:
-        answer += word
-    return answer
+    answer = sorted(s, reverse = True)
+    return ''.join(answer)

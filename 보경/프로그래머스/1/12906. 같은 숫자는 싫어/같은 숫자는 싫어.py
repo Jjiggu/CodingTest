a070dc85e56e384f7cac61b212546428a80d41e4
seobokyeong0314 <116994319+seobokyeong0314@users.noreply.github.com>
2024-08-12 02:37:05 +0900

    Add: [프로그래머스-12906] 같은 숫자는 싫어

---
+++ 보경/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i-1]:
+            arr[i-1] = ''
+    for i in arr:
+        if i != '':
+            answer.append(i)
+    return answer

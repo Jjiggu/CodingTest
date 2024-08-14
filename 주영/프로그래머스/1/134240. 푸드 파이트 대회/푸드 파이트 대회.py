b0331146f2253b8cc85a8b96015acee2fdef1e2d
Ju0011 <yhs07240@naver.com>
2024-08-14 11:31:24 +0900

    Fix : [Programmers-134240] 푸트 파이트 대회

--- 주영/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 주영/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,11 +1,5 @@
 def solution(food):
-    answer = ''
-    for i in range(1,len(food)):
-        answer += str(i)*(food[i]//2)
-    answer += str(0)
-    s1 = list(answer)
-    s1.reverse()
-    for i in range(1,len(s1)):
-        answer+=s1[i]
-        
-    return answer
+    temp = ''
+    for i in range(1, len(food)):
+        temp += str(i) * (food[i]//2)
+    return temp + '0' + temp[::-1]

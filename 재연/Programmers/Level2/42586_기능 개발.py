56c8f8bb667577bfa0c7dce1abc6ef9a5acdfbd8
jymin99 <min10909@naver.com>
2024-11-22 18:28:02 +0900

    Add:[{programmers}-42586]

--- 재연/Programmers/Level2/42586_기능 개발.py
+++ 재연/Programmers/Level2/42586_기능 개발.py
@@ -2,17 +2,15 @@ import math
 import queue
 def solution(progresses, speeds):
     answer = []
-    process=queue.Queue()
+    process=[]
     for i in range(len(progresses)):
-        process.push(math.ceil((100-progresses[i]/speeds[i])))
-    while(process):
-        for i in range(process.qsize()):
-            
-                    
+        process.append(math.ceil((100 - progresses[i]) / speeds[i]))
+    while process:
+        done=1
+        limit=process.pop(0)
+        while process and process[0]<=limit:
+            process.pop(0)
+            done+=1
+        answer.append(done)
     
-    # return answer
-# while process로 감싸서
-# 상단에서 가장 앞의 수를 저장해서
-# 비교해서 그 수보다 작으면 pop하기
-# 더 큰 수를 마주하는 순간 멈추고 answer에 값 저장
-# 큐가 비워질때까지 반복
+    return answer

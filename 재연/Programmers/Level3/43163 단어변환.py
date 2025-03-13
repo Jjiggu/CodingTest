03e1dd5d73e601a4c30e46321175ab1743a2e3d6
민재연 <73513813+jymin99@users.noreply.github.com>
2025-03-13 19:53:12 +0900

    Update 43163 단어변환.py

--- 재연/Programmers/Level3/43163 단어변환.py
+++ 재연/Programmers/Level3/43163 단어변환.py
@@ -1,22 +1,23 @@
 from collections import deque
-
-def solution(n, computers):
+def solution(begin, target, words):
     answer = 0
-    visited=[0 for i in range(n)]
+    q=deque()
+    q.append([begin,0])
+    visited=[0 for i in range(len(words))]
     
-    def bfs(i):
-        q=deque()
-        q.append(i)
-        while q:
-            i=q.popleft()
-            visited[i]=1
-            for j in range(n):
-                if computers[i][j] and not visited[j]:
-                    q.append(j)
-                    
-    for i in range(n):
-        if not visited[i]:
-            bfs(i)
-            answer+=1
+    while q:
+        word,cnt=q.popleft()
+        if word==target:
+            answer=cnt
+            break
+        for i in range(len(words)):
+            same_cnt=0
+            if not visited[i]:
+                for j in range(len(word)):
+                    if word[j]!=words[i][j]:
+                        same_cnt+=1
+                if same_cnt==1:
+                    q.append([words[i],cnt+1])
+                    visited[i]=1
     
     return answer

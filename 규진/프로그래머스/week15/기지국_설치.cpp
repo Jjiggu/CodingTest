c551361d21b13332e7e0b7ae1df92843ee825762
kjchoi1997 <72608849+kjchoi1997@users.noreply.github.com>
2025-03-13 19:29:35 +0900

    Update 기지국_설치.cpp

--- 규진/프로그래머스/week15/기지국_설치.cpp
+++ 규진/프로그래머스/week15/기지국_설치.cpp
@@ -3,8 +3,7 @@
 #include <cmath>
 using namespace std;
 
-int solution(int n, vector<int> stations, int w)
-{
+int solution(int n, vector<int> stations, int w) {
     int answer = 0;
     int range = 2 * w + 1;
     int idx = 1;

c0897d4e153eb30e2ce72b16d002d2cadc08dfea
kjchoi1997 <72608849+kjchoi1997@users.noreply.github.com>
2025-03-27 13:42:33 +0900

    큰돌 커밋

--- 규진/큰돌/2주차/17298.cpp
+++ 규진/큰돌/2주차/17298.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <stack>
 
 using namespace std;
@@ -10,13 +11,18 @@ stack<int> s;
 
 int main(void) {
     cin >> N;
-    for (int i = 1; i <= N; i++) {
+    fill(&ret[0], &ret[0] + 1000001, -1);
+    for (int i = 0; i < N; i++) {
         cin >> seq[i];
         while (s.size() && seq[s.top()] < seq[i]) {
             ret[s.top()] = seq[i];
             s.pop();
         }
-        s.push(seq[i]);
+        s.push(i);
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << ret[i] << " ";
     }
     return 0;
 }

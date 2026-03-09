245770c844bebe0d305d751c859dc296340a2610
kjchoi1997 <72608849+kjchoi1997@users.noreply.github.com>
2026-03-09 23:14:22 +0900

    커밋

---
+++ 규진/백준/week15/스도쿠.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int map[9][9];
+
+void checkRow() {
+
+}
+
+void checkCol() {
+
+}
+
+void checkSquare() {
+    
+}
+
+int main(void) {
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            int num;
+            cin >> num;
+            map[i][j] = num;
+        }
+    }
+
+
+    return 0;
+}
